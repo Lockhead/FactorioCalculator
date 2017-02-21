51139525d34fc3e648acbf70c4ca0568239f4c17
kakyo <luis.diaspinheiro@gmail.com>
2017-02-21 15:05:01 +0000

    item icon component supports multiple options

--- src/components/item-icon/item-icon.js
+++ src/components/item-icon/item-icon.js
@@ -2,19 +2,34 @@ define(['knockout', 'text!./item-icon.html', 'i18n'], function(ko, template, $i)
 
     function ItemIconViewModel(params) {
         var $self = this;
+
         var id = typeof(params.id) === "function" ? params.id() : params.id;
-        var name = $i(id);
+        $self.id = ko.observable(id);
+
+        var name = ko.pureComputed(() => $i($self.id()));
+
 
         $self.hasValue = !!params.value;
-        $self.displayName = params.displayName ? name : "";
+        $self.displayName = ko.pureComputed(() => params.displayName ? name() : "");
 
         $self.value = params.value;
         $self.readonly = params.readonly;
-        $self.id = id;
         $self.type = params.type || 'text';
         $self.tooltip = name;
 
-        if ($self.displayName && !$self.hasValue) {
+        $self.hasOptions = !!params.valueOptions && params.valueOptions.length > 1;
+        $self.optionsAvailable = [];
+        if ($self.hasOptions) {
+            for (var i = 0; i < params.valueOptions.length; i++) {
+                var opt = params.valueOptions[i];
+                $self.optionsAvailable.push({
+                    id: opt,
+                    name: $i(opt)
+                });
+            }
+        }
+
+        if ($self.displayName() && !$self.hasValue) {
             $self.value = name;
             $self.hasValue = 1;
             $self.readonly = 1;
@@ -23,7 +38,10 @@ define(['knockout', 'text!./item-icon.html', 'i18n'], function(ko, template, $i)
     }
 
     ItemIconViewModel.prototype.getIconName = function() {
-        return 'factorio-' + this.id;
+        return 'factorio-' + this.id();
+    }
+    ItemIconViewModel.prototype.setOption = function(selectedBuilding) {
+        this.id(selectedBuilding.id);
     }
 
     return {
