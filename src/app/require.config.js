5b50c7e25634d7eba544bea06e6e40ebb7752142
kakyo <luis.diaspinheiro@gmail.com>
2017-04-27 02:03:52 +0000

    included sprite sheet and translation for v15 new itens ' q

--- src/app/require.config.js
+++ src/app/require.config.js
@@ -3,20 +3,34 @@ var require = {
     baseUrl: ".",
     urlArgs: "bust=" + (+new Date),
     paths: {
-        "bootstrap":            "bower_modules/components-bootstrap/js/bootstrap.min",
-        "crossroads":           "bower_modules/crossroads/dist/crossroads.min",
-        "hasher":               "bower_modules/hasher/dist/js/hasher.min",
-        "jquery":               "bower_modules/jquery/dist/jquery",
-        "text":                 "bower_modules/requirejs-text/text",
-        "signals":              "bower_modules/js-signals/dist/signals.min",
-        "knockout":             "bower_modules/knockout/dist/knockout.debug",
+        "bootstrap": "bower_modules/components-bootstrap/js/bootstrap.min",
+        "crossroads": "bower_modules/crossroads/dist/crossroads.min",
+        "hasher": "bower_modules/hasher/dist/js/hasher.min",
+        "jquery": "bower_modules/jquery/dist/jquery",
+        "text": "bower_modules/requirejs-text/text",
+        "signals": "bower_modules/js-signals/dist/signals.min",
+        "knockout": "bower_modules/knockout/dist/knockout.debug",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
-        "knockout-extensions":  "app/knockout-extensions",
-        "js-extensions":        "app/extensions",
-        "data":                 "app/dataLoader",
-        "i18n":                 "app/i18n"
+        "knockout-extensions": "app/knockout-extensions",
+        "js-extensions": "app/extensions",
+        "data": "app/dataLoader",
+        "i18n": "app/i18n"
     },
     shim: {
-        "bootstrap": { deps: ["jquery"] }
+        "bootstrap": {
+            deps: ["jquery"]
+        }
     }
-};
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    //console.log('Query variable %s not found', variable);
+}
